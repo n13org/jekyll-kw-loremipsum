fce15eb8d33d3f17e722bcaa76a8e3195c0fc871
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2022-03-21 12:17:15 +0100

    Add class `ParserParameter`

---
+++ lib/jekyll-kw-loremipsum/parser_parameter.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module KargWare
+    module LoremIpsum
+      # Parameters for parser class
+      class ParserParameter
+        attr_reader :words, :paras, :minw, :maxw, :random
+
+        def initialize
+          super
+
+          # 1w    ==  1 word (default 40)
+          @words = 40
+          # 3p    ==  3 paragraphs (default 1)
+          # 7miw  ==  7 Min words in para (default 40)
+          # 15maw == 15 Max words in para (default 40)
+          @paras = 1
+          @minw = 40
+          @maxw = 40
+          # random == Should the words be random (or ordered) (default false)
+          @random = false
+        end
+
+        def init(parameters = nil)
+          return if parameters.nil?
+
+          # TODO: Min > Max --> Warning
+          # TODO: unkown words, e.g. "p, word, min, max" --> Warning
+
+          matched_words = parameters.strip.match(/\d+w/)
+          matched_paras = parameters.strip.match(/\d+p/)
+          matched_minw = parameters.strip.match(/\d+miw/)
+          matched_maxw = parameters.strip.match(/\d+maw/)
+          matched_random = parameters.strip.match(/random/)
+
+          # puts "Parser: W: %s P: %s MiW: %s MaW: %s Random: $s" % [matched_words, matched_paras, matched_minw, matched_maxw, matched_random]
+
+          @words = matched_words[0].strip.sub('w', '').to_i if matched_words
+          @paras = matched_paras[0].strip.sub('p', '').to_i if matched_paras
+          @minw = matched_minw[0].strip.sub('miw', '').to_i if matched_minw
+          @maxw = matched_maxw[0].strip.sub('maw', '').to_i if matched_maxw
+          @random = true if matched_random
+        end
+
+        def to_str_random
+          @random ? 'random' : 'sorted'
+        end
+
+        def use_words
+          @paras.nil? || @paras == 1
+        end
+
+        def use_paragraphs
+          !use_words
+        end
+
+        def to_s
+          if use_words
+            format('W: %04d (%s)', @words, to_str_random)
+          else
+            format('P: %04d (%s) MiW: %04d MaW: %04d', @paras, to_str_random, @minw, @maxw)
+          end
+        end
+
+        def echo
+          puts to_s
+        end
+      end
+    end
+  end
+end
---
+++ test/jekyll-kw-loremipsum/parser_parameter_test.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'jekyll-kw-loremipsum/parser_parameter'
+
+module Jekyll
+  module KargWare
+    module LoremIpsum
+      class ParserParameterTest < Minitest::Test
+        def test_ctor_default
+          sut = ParserParameter.new
+
+          assert_equal 40, sut.words
+          assert_equal 1, sut.paras
+          assert_equal 40, sut.minw
+          assert_equal 40, sut.maxw
+          assert_equal false, sut.random
+
+          assert_equal 'W: 0040 (sorted)', sut.to_s
+        end
+
+        def test_ctor_argument_error
+          assert_raises ArgumentError do
+            ParserParameter.new('40w')
+          end
+        end
+
+        def test_ctor_init_nil
+          sut = ParserParameter.new
+          sut.init(nil)
+
+          assert_equal 40, sut.words
+          assert_equal 1, sut.paras
+          assert_equal 40, sut.minw
+          assert_equal 40, sut.maxw
+          assert_equal false, sut.random
+
+          assert_equal 'W: 0040 (sorted)', sut.to_s
+        end
+
+        def test_ctor_with_init_25w
+          sut = ParserParameter.new
+          sut.init('25w')
+
+          assert_equal 25, sut.words
+          assert_equal 1, sut.paras
+          assert_equal 40, sut.minw
+          assert_equal 40, sut.maxw
+          assert_equal false, sut.random
+
+          assert_equal 'W: 0025 (sorted)', sut.to_s
+        end
+
+        def test_ctor_with_init_2p_5miw_13maw_random
+          sut = ParserParameter.new
+          sut.init('2p 5miw 13maw random')
+
+          assert_equal 40, sut.words
+          assert_equal 2, sut.paras
+          assert_equal 5, sut.minw
+          assert_equal 13, sut.maxw
+          assert_equal true, sut.random
+
+          assert_equal 'P: 0002 (random) MiW: 0005 MaW: 0013', sut.to_s
+        end
+
+        def test_usage_of_words
+          sut = ParserParameter.new
+          sut.init('25w')
+
+          assert_equal true, sut.use_words
+          assert_equal false, sut.use_paragraphs
+        end
+
+        def test_usage_of_paragraphs
+          sut = ParserParameter.new
+          sut.init('4p')
+
+          assert_equal false, sut.use_words
+          assert_equal true, sut.use_paragraphs
+        end
+      end
+    end
+  end
+end
