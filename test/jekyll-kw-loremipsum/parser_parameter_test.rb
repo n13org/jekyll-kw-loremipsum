d16e2cc989b448a064714c3258b270b772155994
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2022-11-02 06:55:40 +0100

    #20 Use require_relative for test_helper

--- test/jekyll-kw-loremipsum/parser_parameter_test.rb
+++ test/jekyll-kw-loremipsum/parser_parameter_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require_relative '../test_helper'
 require 'jekyll-kw-loremipsum/parser_parameter'
 
 module Jekyll
--- test/jekyll-kw-loremipsum/parser_test.rb
+++ test/jekyll-kw-loremipsum/parser_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require_relative '../test_helper'
 require 'jekyll-kw-loremipsum/configuration'
 require 'jekyll-kw-loremipsum/parser'
 
